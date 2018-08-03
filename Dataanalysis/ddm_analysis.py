1afea26e2aadcbd714f683cd67016d2a09dd8c44
mcvinding <mikkel.vinding@gmail.com>
2018-08-04 00:06:29 +0200

    Added a new "null"-model to DDM and calculate HPD

--- Dataanalysis/ddm_analysis.py
+++ Dataanalysis/ddm_analysis.py
@@ -7,6 +7,7 @@ Note: Run in py 2.7
 import hddm
 import os.path as op
 from os import chdir
+import os.path as op
 import pandas as pd
 import matplotlib.pyplot as plt
 from kabuki.analyze import gelman_rubin
@@ -46,6 +47,11 @@ mod0.find_starting_values()
 mod0.sample(10000, burn=2000, dbname='traces0.db', db='pickle')
 mod0.save(op.join(outdir,'ddf_model0'))
 
+modz = hddm.HDDM(data, depends_on ={'v': 'type', 'a':'type'})
+modz.find_starting_values()
+modz.sample(10000, burn=2000, dbname='traces0.db', db='pickle')
+modz.save(op.join(outdir,'ddf_modelZ'))
+
 modv = hddm.HDDM(data, depends_on ={'v': ['type','volition'], 'a':'type'})
 modv.find_starting_values()
 modv.sample(10000, burn=2000, dbname='traces.db', db='pickle')
@@ -74,17 +80,24 @@ for i in range(5):
     m.sample(10000, burn=2000)
     models.append(m)
 
-gelman_rubin(models)
-
-with open('models.txt','wb') as fb:
+with open(op.join(outdir,'models.pkl'),'wb') as fb:
     pickle.dump(models,fb)
+    
+#with open(op.join(outdir,'models.pkl'), 'rb') as f:
+#        pickle.load(f)  
+
+gelman_rubin(models) 
 
 # %% Summary and plots
-mod = hddm.load(op.join(workdir,'ddf_model'))
+mod = hddm.load(op.join(outdir,'ddf_model'))
 #modz = hddm.load(op.join(outdir,'ddf_modelZ'))
 
 # Compare DIC
-
+print 'DIC full model = ', mod.dic
+print 'DIC null model = ', mod0.dic
+print 'DIC type model = ', modz.dic
+print 'DIC a model = ', moda.dic
+print 'DIC v model = ', modv.dic
 
 # Get statistics from posterior
 stats = mod.gen_stats()
@@ -92,10 +105,12 @@ mod.print_stats()
 mod.get_group_nodes()
 
 # Posterior checks
-mod.plot_posterior_predictive()
+#mod.plot_posterior_predictive()
+mod.plot_posterior_quantiles()
 
+# Plot posteriors
 v_fixPM, v_freePM, v_fixFil, v_freeFil  = mod.nodes_db.node[['v(pm.fix)', 'v(pm.free)','v(filler.fix)','v(filler.free)']]
-hddm.analyze.plot_posterior_nodes([v_fixPM, v_freePM, v_fixFil, v_freeFil], lb=1.4, ub=3.0)
+hddm.analyze.plot_posterior_nodes([v_fixPM, v_freePM, v_fixFil, v_freeFil], lb=1.6, ub=3.3)
 plt.ylabel('Density')
 plt.xlabel('Value')
 plt.title('Posterior: $\it{v}$')
@@ -103,14 +118,44 @@ plt.legend(['Fixed: PM-cue','Free: PM-cue','Fixed: Filler','Free: Filler'], font
 plt.savefig('v_post')
 
 a_fixPM, a_freePM, a_fixFil, a_freeFil  = mod.nodes_db.node[['a(pm.fix)', 'a(pm.free)','a(filler.fix)','a(filler.free)']]
-hddm.analyze.plot_posterior_nodes([a_fixPM, a_freePM, a_fixFil, a_freeFil], lb=1.4, ub=3.0)
+hddm.analyze.plot_posterior_nodes([a_fixPM, a_freePM, a_fixFil, a_freeFil], lb=1.0, ub=2.5)
 plt.ylabel('Density')
 plt.xlabel('Value')
 plt.title('Posterior: $\it{a}$')
 plt.legend(['Fixed: PM-cue','Free: PM-cue','Fixed: Filler','Free: Filler'], fontsize=8, loc=0, edgecolor='white')
 plt.savefig('a_post')
 
-print "P_v(Free < Fix) = ", (v_fixPM.trace() > v_freePM.trace()).mean()
-print "P_v(Free < Fix) = ", (v_fixFil.trace() > v_freeFil.trace()).mean()
-print "P_a(Free < Fix) = ", (a_fixPM.trace() > a_freePM.trace()).mean()
-print "P_a(Free < Fix) = ", (a_fixFil.trace() > a_freeFil.trace()).mean()
+# Comparison of posteriors
+print "PM: P_v(Free < Fix) = ", (v_fixPM.trace() < v_freePM.trace()).mean()
+print "Fil: P_v(Free < Fix) = ", (v_fixFil.trace() < v_freeFil.trace()).mean()
+print "PM: P_a(Free < Fix) = ", (a_fixPM.trace() < a_freePM.trace()).mean()
+print "Fil: P_a(Free < Fix) = ", (a_fixFil.trace() < a_freeFil.trace()).mean()
+
+
+#%% HDI
+import pymc as pc  
+print(pc.utils.hpd(mod.get_traces()['a(filler.fix)'], 0.05))
+print(pc.utils.hpd(mod.get_traces()['a(filler.free)'], 0.05))
+print(pc.utils.hpd(mod.get_traces()['a(pm.fix)'], 0.05))
+print(pc.utils.hpd(mod.get_traces()['a(pm.free)'], 0.05))
+
+print(pc.utils.hpd(mod.get_traces()['v(filler.fix)'], 0.05))
+print(pc.utils.hpd(mod.get_traces()['v(filler.free)'], 0.05))
+print(pc.utils.hpd(mod.get_traces()['v(pm.fix)'], 0.05))
+print(pc.utils.hpd(mod.get_traces()['v(pm.free)'], 0.05))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
